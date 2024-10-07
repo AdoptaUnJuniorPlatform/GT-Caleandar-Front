c182ead513c75fbd9824890606433415ec032de0
LuciaM <luquiceno@gmail.com>
2024-10-07 20:14:29 +0200

    Adding the login form

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -5,7 +5,54 @@ export default {
     './node_modules/flowbite/**/*.js'
   ],
 	theme: {
-		extend: {},
+		extend: {
+      colors: {
+        primary: {
+          "50":"#eff6ff",
+          "100":"#dbeafe",
+          "200":"#bfdbfe",
+          "300":"#93c5fd",
+          "400":"#151a20",
+          "500":"#39b3c2"
+        }
+      }
+    },
+    fontFamily: {
+      'body': [
+      'Inter', 
+      'ui-sans-serif', 
+      'system-ui', 
+      '-apple-system', 
+      'system-ui', 
+      'Segoe UI', 
+      'Roboto', 
+      'Helvetica Neue', 
+      'Arial', 
+      'Noto Sans', 
+      'sans-serif', 
+      'Apple Color Emoji', 
+      'Segoe UI Emoji', 
+      'Segoe UI Symbol', 
+      'Noto Color Emoji'
+      ],
+      'sans': [
+        'Inter', 
+        'ui-sans-serif', 
+        'system-ui', 
+        '-apple-system', 
+        'system-ui', 
+        'Segoe UI', 
+        'Roboto', 
+        'Helvetica Neue', 
+        'Arial', 
+        'Noto Sans', 
+        'sans-serif', 
+        'Apple Color Emoji', 
+        'Segoe UI Emoji', 
+        'Segoe UI Symbol', 
+        'Noto Color Emoji'
+      ]
+    }
 	},
 	plugins: [
     require('@tailwindcss/typography'),
