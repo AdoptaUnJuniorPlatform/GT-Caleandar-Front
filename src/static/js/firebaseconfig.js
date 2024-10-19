f836d49f83e6e17eda00a2ec04d268feb45817f1
joams mogro gomez <joamsmg@gmail.com>
2024-10-18 22:42:23 -0400

    fix: now environment variables were declared in .env for security

--- src/static/js/firebaseconfig.js
+++ src/static/js/firebaseconfig.js
@@ -3,14 +3,14 @@ import { getAuth, GoogleAuthProvider, setPersistence, browserLocalPersistence } 
 import { getDatabase } from 'firebase/database';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCNvRFL-YT9Pby9u5Oz8Z_cTr6kZRX5rKk",
-    authDomain: "caleandar-leanmind.firebaseapp.com",
-    databaseURL: "https://caleandar-leanmind-default-rtdb.europe-west1.firebasedatabase.app",
-    projectId: "caleandar-leanmind",
-    storageBucket: "caleandar-leanmind.appspot.com",
-    messagingSenderId: "61171199093",
-    appId: "1:61171199093:web:0d91f9edb0594de9cd1c3c",
-    measurementId: "G-C3JM55JMXL"
+    apiKey: import.meta.env.PUBLIC_API_KEY,
+    authDomain: import.meta.env.PUBLIC_AUTH_DOMAIN,
+    databaseURL: import.meta.env.PUBLIC_DATABASE_URL,
+    projectId: import.meta.env.PUBLIC_PROJECT_ID,
+    storageBucket: import.meta.env.PUBLIC_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.PUBLIC_MESSAGING_SENDER_ID,
+    appId: import.meta.env.PUBLIC_APP_ID,
+    measurementId: import.meta.env.PUBLIC_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
